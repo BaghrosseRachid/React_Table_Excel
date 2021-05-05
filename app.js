7b1e71e4c0dc4d74226fd85c6a7189261c50882f
rachid <rachid98baghrosse@gmail.com>
2021-05-05 16:41:48 +0200

    m

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 
   // get all students
-  app.get('/api/students', (req, res,next) => {
+  app.get('/api/students', (req, res) => {
   
       try{
         const students = [
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const normalizePort = val => {
   }
   return false;
 };
+
 //for production
 if (process.env.NODE_ENV === 'production') {
   //set satatic folder
